90d0685ebe45ee10e4cd1e3df1928114ea11a58d
Vagner Ferreira Santos <vagner.santos@enext.com.br>
2017-04-17 10:31:08 -0300

    fixing task runner configurations

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var concat = require("gulp-concat");
 var uglify = require('gulp-uglify');
 var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
 var gulpDocumentation = require('gulp-documentation');
 
+// DEFAULT TASK
+// use it in develoment
 gulp.task('default', function() {
-    gulp.src([
+    return gulp.src([
         'src/components/**/*.js',
         'src/core/**/*.js',
         'src/api/**/*.js',
         'src/tests/**/*.js'
     ])
-    .pipe(
-        plumber({
-            errorHandler: notify.onError("Error: <%= error.message %>")
-        })
-    )
-    .pipe(concat('enext-wishlist.js'))
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(concat('vtex-custom-wishlist.js'))
     .pipe(notify({
         title: "JS READY",
         message: "Generate file: <%= file.relative %>!"
@@ -25,32 +25,49 @@ gulp.task('default', function() {
     .pipe(gulp.dest('build/'));
 });
 
-gulp.task('build', function() {
-    gulp.src(['src/core/**/*.js', 'src/components/**/*.js'])
-        .pipe(
-            plumber({
-                errorHandler: notify.onError("Error: <%= error.message %>")
-            })
-        )
-        .pipe(uglify())
-        .pipe(concat('enext-wishlist.js'))
-        .pipe(notify({
-            title: "JS READY",
-            message: "Generate file: <%= file.relative %>!"
-        }))
-        .pipe(gulp.dest('build/'));
+// DIST TASK
+// use to make your deploy
+gulp.task('dist', ['doc', 'cleanDist'], function() {
+    return gulp.src([
+        'src/components/**/*.js',
+        'src/core/**/*.js',
+        'src/api/**/*.js'
+    ])
+    .pipe(plumber(function(error){
+        notify({
+            title: "JS ERROR",
+            message: error.toString()
+        })
+    }))
+    .pipe(concat('vtex-custom-wishlist.min.js'))
+    .pipe(notify({
+        title: "JS READY",
+        message: "Generate file: <%= file.relative %>!"
+    }))
+    .pipe(gulp.dest('dist/'));
 });
 
-// Generating a pretty HTML documentation site
+// DOC TASK
+// use to generate the documentation
 gulp.task('doc', function () {
-  return gulp.src('./src/**/*.js')
-    .pipe(gulpDocumentation('html'))
-    .pipe(gulp.dest('html-documentation'));
+    return gulp.src('./src/**/*.js')
+        .pipe(plumber())
+        .pipe(gulpDocumentation('md'))
+        .pipe(gulp.dest('./'));
 });
 
+// CLEAN TASK
+// use clean the dist before generate it again
+gulp.task('cleanDist', function(){
+    return gulp.src('./dist')
+            .pipe(clean());
+});
+
+// WATCH TASK
+// use to keep yourself developing
 gulp.task('watch', function(){
-    gulp.watch('src/**/*.js', ['default', 'doc'])
+    return gulp.watch('src/**/*.js', ['default'])
         .on('change', function(event){
-            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        });
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });
