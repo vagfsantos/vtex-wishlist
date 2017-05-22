ae4a07df416f9be999d3396fe938eac2e910e422
Vagner Ferreira Santos <vagner.santos@enext.com.br>
2017-05-22 09:22:19 -0300

    changing namespace

--- src/tests/Test.js
+++ src/tests/Test.js
@@ -9,39 +9,39 @@ function it(description, callback){
 }
 
 it('should add an id to storage', function(){
-    WISHLIST.add('15');
-    var result = WISHLIST.getIds();
+    vtexCustomWishlist.add('15');
+    var result = vtexCustomWishlist.getIds();
     if( result.indexOf('15') >= 0 ){
         return true;
     }
 });
 
 it('should return all ids into the storage', function(){
-    var result = WISHLIST.getIds();
+    var result = vtexCustomWishlist.getIds();
     if( Array.isArray(result) ){
         return true;
     }
 });
 
 it('should remove an id to storage', function(){
-    WISHLIST.delete('15');
-    var result = WISHLIST.getIds();
+    vtexCustomWishlist.delete('15');
+    var result = vtexCustomWishlist.getIds();
     if( result.indexOf('15') === -1 ){
         return true;
     }
 });
 
 it('should clean all storage', function(){
-    WISHLIST.clean();
-    var result = WISHLIST.getIds();
+    vtexCustomWishlist.clean();
+    var result = vtexCustomWishlist.getIds();
     if( result.length === 0 ){
         return true;
     }
 });
 
 it('should not allow 2 equal ids in the store', function(){
-    WISHLIST.add('15').add('15');
-    var result = WISHLIST.getIds();
+    vtexCustomWishlist.add('15').add('15');
+    var result = vtexCustomWishlist.getIds();
     var count = 0;
     
     result.forEach(function(item){
@@ -56,11 +56,11 @@ it('should not allow 2 equal ids in the store', function(){
 });
 
 it('should config properly the app', function(){
-    WISHLIST.config({
+    vtexCustomWishlist.config({
         test: 'test'
     });
     
-    var result = WISHLIST.getConfig();
+    var result = vtexCustomWishlist.getConfig();
     
     if( result.test === 'test' ){
         return true;
@@ -68,14 +68,14 @@ it('should config properly the app', function(){
 });
 
 
-WISHLIST.clean();
+vtexCustomWishlist.clean();
 for(var i = 0; i < 5; i++){
     (function(i){
         setTimeout(function(){
-            WISHLIST.add(i);
+            vtexCustomWishlist.add(i);
             if( i === 4 ){
                 it('should save various products in a row', function(){
-                    return WISHLIST.getIds().length === 5;
+                    return vtexCustomWishlist.getIds().length === 5;
                 })
             }
         }, 400 * i);
